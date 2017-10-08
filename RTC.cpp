c9038ec391476a829903a6408779ffba00fbb6b1
MootSeeker <mootseeker98@gmail.com>
2017-10-08 03:29:02 +0200

    Add Library Files

---
+++ RTC.cpp
@@ -0,0 +1,442 @@
+/*
+ *
+ *  Description:
+ *  -------------
+ *
+ *    project name: RTC 
+ *    File Name: RTC.cpp
+ *
+ *
+ *    Version:		Date:        Author:         comment:
+ *    --------		-----------  --------------  -------------------
+ *    1.00			07.10.2017   Kevin Perillo   build.
+ *
+ *
+ *    Copyright (c) MootSeeker 2017
+ ***********************************************************************************/
+
+#include "RTC.h"
+
+
+/*=========================================================================
+    Get Seconds from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getSecond(void) 
+{
+	Wire.beginTransmission(Clock_ADDRESS);	// Begin Transmission with the Clock (Address = 0x68).
+	Wire.write(DS3231_REGISTER_Second);		// Write Adress of the Register. 
+	Wire.endTransmission();					// End Transmission. 
+
+	Wire.requestFrom(Clock_ADDRESS, 1);		// Send Stop-Bit
+	return bcdToDec(Wire.read());			// Read Register Value and convert to DEC. 
+}
+
+/*=========================================================================
+    Get Minutes from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getMinute(void) 
+{
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Minute);
+	Wire.endTransmission();
+
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	return bcdToDec(Wire.read());
+}
+
+/*=========================================================================
+    Get Hours from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getHour(bool& h12, bool& PM)		// Set 12/24h mode  | Activate AM/PM. 
+{
+	uint8_t temp_buffer;
+	uint8_t hour;
+	
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Hour);
+	Wire.endTransmission();
+
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	temp_buffer = Wire.read();
+	
+	h12 = (temp_buffer & 0x40);
+	if (h12) 
+	{
+		PM = (temp_buffer & 0x20);
+		hour = bcdToDec(temp_buffer & 0x1F);
+	} 
+	else 
+	{
+		hour = bcdToDec(temp_buffer & 0b3F);
+	}
+	return hour;
+}
+
+/*=========================================================================
+    Get Day of Week from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getDoW(void) 
+{
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_DoW);
+	Wire.endTransmission();
+
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	return bcdToDec(Wire.read());
+}
+
+/*=========================================================================
+    Get Date (Day) from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getDate(void) 
+{
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Date);
+	Wire.endTransmission();
+
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	return bcdToDec(Wire.read());
+}
+
+/*=========================================================================
+    Get Month from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getMonth(bool& Century) 
+{
+	uint8_t temp_buffer;
+	uint8_t hour;
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Month);
+	Wire.endTransmission();
+
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	temp_buffer = Wire.read();
+	
+	Century = temp_buffer & 0x80;
+	return (bcdToDec(temp_buffer & 0xEF));
+}
+
+/*=========================================================================
+    Get Year from the Register.
+  -----------------------------------------------------------------------*/
+uint8_t getYear(void) 
+{
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Year);
+	Wire.endTransmission();
+
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	return bcdToDec(Wire.read());
+}
+
+/*=========================================================================
+    Set Second in Register.
+  -----------------------------------------------------------------------*/
+void setSecond(uint8_t Second) 
+{
+	// Sets the seconds 
+	// This function also resets the Oscillator Stop Flag, which is set
+	// whenever power is interrupted.
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Second);
+	Wire.write(decToBcd(Second));	
+	Wire.endTransmission();
+	
+	// Clear OSF flag
+	uint8_t temp_buffer = readControlByte(1);
+	writeControlByte((temp_buffer & 0xEF), 1);
+}
+
+/*=========================================================================
+    Set Minutes in Register.
+  -----------------------------------------------------------------------*/
+void setMinute(uint8_t Minute) 
+{
+	// Sets the minutes 
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Minute);
+	Wire.write(decToBcd(Minute));	
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Set Hour in Register.
+  -----------------------------------------------------------------------*/
+void setHour(uint8_t Hour) 
+{
+	// Sets the hour, without changing 12/24h mode.
+	// The hour must be in 24h format.
+
+	bool h12;
+
+	// Start by figuring out what the 12/24 mode is
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Hour);
+	Wire.endTransmission();
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	
+	h12 = (Wire.read() & 0b01000000);
+	// if h12 is true, it's 12h mode; false is 24h.
+
+	if (h12) {
+		// 12 hour
+		if (Hour > 12) {
+			Hour = decToBcd(Hour-12) | 0b01100000;
+		} else {
+			Hour = decToBcd(Hour) & 0b11011111;
+		}
+	} else {
+		// 24 hour
+		Hour = decToBcd(Hour) & 0b10111111;
+	}
+
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Hour);
+	Wire.write(Hour);
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Set Day of Week in Register.
+  -----------------------------------------------------------------------*/
+void setDoW(uint8_t DoW) 
+{
+	// Sets the Day of Week
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_DoW);
+	Wire.write(decToBcd(DoW));	
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Set Date (Day) in Register.
+  -----------------------------------------------------------------------*/
+void setDate(uint8_t Date) 
+{
+	// Sets the Date
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Date);
+	Wire.write(decToBcd(Date));	
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Set Month in Register.
+  -----------------------------------------------------------------------*/
+void setMonth(uint8_t Month) 
+{
+	// Sets the month
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Month);
+	Wire.write(decToBcd(Month));	
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Set Year in Register.
+  -----------------------------------------------------------------------*/
+void setYear(uint8_t Year) 
+{
+	// Sets the year
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Year);
+	Wire.write(decToBcd(Year));	
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Set Clock Mode in Register.
+  -----------------------------------------------------------------------*/
+void setClockMode(bool h12) 
+{
+	// sets the mode to 12-hour (true) or 24-hour (false).
+	// One thing that bothers me about how I've written this is that
+	// if the read and right happen at the right hourly millisecnd,
+	// the clock will be set back an hour. Not sure how to do it better, 
+	// though, and as long as one doesn't set the mode frequently it's
+	// a very minimal risk. 
+	// It's zero risk if you call this BEFORE setting the hour, since
+	// the setHour() function doesn't change this mode.
+	
+	uint8_t temp_buffer;
+
+	// Start by reading byte 0x02.
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Hour);
+	Wire.endTransmission();
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	temp_buffer = Wire.read();
+
+	// Set the flag to the requested value:
+	if (h12) 
+	{
+		temp_buffer = temp_buffer | 0x01000000;
+	} 
+	else 
+	{
+		temp_buffer = temp_buffer & 0x10111111;
+	}
+
+	// Write the byte
+	Wire.beginTransmission(Clock_ADDRESS);
+	Wire.write(DS3231_REGISTER_Hour);
+	Wire.write(temp_buffer);
+	Wire.endTransmission();
+}
+
+/*=========================================================================
+    Get Temperature in Register.
+  -----------------------------------------------------------------------*/
+float getTemperature() 
+{
+	// Checks the internal thermometer on the DS3231 and returns the 
+	// temperature as a floating-point value.
+  
+  uint8_t tMSB, tLSB;
+  float temp3231;
+  
+  // temp registers (11h-12h) get updated automatically every 64s
+  Wire.beginTransmission(Clock_ADDRESS);
+  Wire.write(DS3231_REGISTER_MSB_Temp);
+  Wire.endTransmission();
+  Wire.requestFrom(Clock_ADDRESS, 2);
+
+  // Should I do more "if available" checks here?
+  if(Wire.available()) 
+  {
+    tMSB = Wire.read(); //2's complement int portion
+    tLSB = Wire.read(); //fraction portion
+
+    temp3231 = ((((short)tMSB << 8) | (short)tLSB) >> 6) / 4.0;
+  }
+  else 
+  {
+    temp3231 = -9999; // Some obvious error value
+  }  
+  return temp3231;
+}
+
+/*=========================================================================
+    Enable Oscillator in Register.
+  -----------------------------------------------------------------------*/
+void enableOscillator(bool TF, bool battery, uint8_t frequency) 
+{
+	// turns oscillator on or off. True is on, false is off.
+	// if battery is true, turns on even for battery-only operation,
+	// otherwise turns off if Vcc is off.
+	// frequency must be 0, 1, 2, or 3.
+	// 0 = 1 Hz
+	// 1 = 1.024 kHz
+	// 2 = 4.096 kHz
+	// 3 = 8.192 kHz (Default if frequency byte is out of range)
+	if (frequency > 3) frequency = 3;
+	// read control byte in, but zero out current state of RS2 and RS1.
+	uint8_t temp_buffer = readControlByte(0) & 0b11100111;
+	if (battery) 
+	{
+		// turn on BBSQW flag
+		temp_buffer = temp_buffer | 0b01000000;
+	} 
+	else 
+	{
+		// turn off BBSQW flag
+		temp_buffer = temp_buffer & 0b10111111;
+	}
+	if (TF) 
+	{
+		// set ~EOSC to 0 and INTCN to zero.
+		temp_buffer = temp_buffer & 0b01111011;
+	} 
+	else 
+	{
+		// set ~EOSC to 1, leave INTCN as is.
+		temp_buffer = temp_buffer | 0b10000000;
+	}
+	// shift frequency into bits 3 and 4 and set.
+	frequency = frequency << 3;
+	temp_buffer = temp_buffer | frequency;
+	// And write the control bits
+	writeControlByte(temp_buffer, 0);
+}
+
+void enable32kHz(bool TF) 
+{
+	// turn 32kHz pin on or off
+	uint8_t temp_buffer = readControlByte(1);
+	if (TF) 
+	{
+		// turn on 32kHz pin
+		temp_buffer = temp_buffer | 0b00001000;
+	} 
+	else 
+	{
+		// turn off 32kHz pin
+		temp_buffer = temp_buffer & 0b11110111;
+	}
+	writeControlByte(temp_buffer, 1);
+}
+
+bool oscillatorCheck() 
+{
+	// Returns false if the oscillator has been off for some reason.
+	// If this is the case, the time is probably not correct.
+	uint8_t temp_buffer = readControlByte(1);
+	bool result = true;
+	if (temp_buffer & 0b10000000) 
+	{
+		// Oscillator Stop Flag (OSF) is set, so return false.
+		result = false;
+	}
+	return result;
+}
+
+
+uint8_t decToBcd(uint8_t val) 
+{
+// Convert normal decimal numbers to binary coded decimal
+	return ((val/10*16) + (val%10));
+}
+
+uint8_t bcdToDec(uint8_t val) 
+{
+// Convert binary coded decimal to normal decimal numbers
+	return ((val/16*10) + (val%16));
+}
+
+uint8_t readControlByte(bool which) 
+{
+	// Read selected control byte
+	// first byte (0) is 0x0e, second (1) is 0x0f
+	Wire.beginTransmission(Clock_ADDRESS);
+	if (which) 
+	{
+		// second control byte
+		Wire.write(0x0f);
+	} 
+	else 
+	{
+		// first control byte
+		Wire.write(0x0e);
+	}
+	Wire.endTransmission();
+	Wire.requestFrom(Clock_ADDRESS, 1);
+	return Wire.read();	
+}
+
+void writeControlByte(uint8_t control, bool which) 
+{
+	// Write the selected control byte.
+	// which=false -> 0x0e, true->0x0f.
+	Wire.beginTransmission(Clock_ADDRESS);
+	if (which) 
+	{
+		Wire.write(0x0f);
+	} 
+	else 
+	{
+		Wire.write(0x0e);
+	}
+	
+	Wire.write(control);
+	Wire.endTransmission();
+}
---
+++ RTC.h
@@ -0,0 +1,194 @@
+/*
+ *
+ *  Description:
+ *  -------------
+ *
+ *    project name: RTC 
+ *    File Name: RTC.h
+ *
+ *
+ *    Version:		Date:        Author:         comment:
+ *    --------		-----------  --------------  -------------------
+ *    1.00			07.10.2017   Kevin Perillo   build.
+ *
+ *
+ *    Copyright (c) MootSeeker 2017
+/*=========================================================================*/
+
+/* 
+	This is a Header-File for the DS3231 Real Time Clock Module from Adafruit.
+*/
+
+#ifndef __RTC_H__
+#define __RTC_H__
+
+	#include "Arduino.h"
+	#include <Wire.h>
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+    #define Clock_ADDRESS                (0x68)
+/*=========================================================================*/
+
+/*=========================================================================
+    REGISTERS
+    -----------------------------------------------------------------------*/
+	#define DS3231_REGISTER_Second   = 0x00, // Seconds Register
+	#define DS3231_REGISTER_Minute   = 0x01, // Minutes Register
+	#define DS3231_REGISTER_Hour	 = 0x02, // Hours Register
+	#define DS3231_REGISTER_DoW		 = 0x03, // Day of the Week Register
+	#define DS3231_REGISTER_Date	 = 0x04, // Date (Day) Register
+	#define DS3231_REGISTER_Month	 = 0x05, // Month Register
+	#define DS3231_REGISTER_Year	 = 0x06, // Year Register
+	#define DS3231_REGISTER_Control  = 0x0E, // Control Register
+	#define DS3231_REGISTER_MSB_Temp = 0x11, // MSB TEMP Register
+	#define DS3231_REGISTER_LSB_Temp = 0x12  // LSB TEMP Register
+  
+/*=========================================================================*/
+
+/*=========================================================================
+    the get*() functions retrieve current values of the registers.
+    -----------------------------------------------------------------------*/
+
+	uint8_t getSecond(void); 
+	uint8_t getMinute(void); 
+	uint8_t getHour(bool& h12, bool& PM); 
+		// In addition to returning the hour register, this function
+		// returns the values of the 12/24-hour flag and the AM/PM flag.
+	uint8_t getDoW(void); 
+	uint8_t getDate(void); 
+	uint8_t getMonth(bool& Century); 
+		// Also sets the flag indicating century roll-over.
+	uint8_t getYear(void); 
+		// Last 2 digits only
+			
+/*=========================================================================*/
+
+/*=========================================================================
+    Time-setting functions.
+    -----------------------------------------------------------------------*/
+
+	// Note that none of these check for sensibility: You can set the
+	// date to July 42nd and strange things will probably result.
+	
+	void setSecond(uint8_t Second); 
+		// Sets the seconds
+	void setMinute(uint8_t Minute); 
+		// Sets the minute
+	void setHour(uint8_t Hour); 
+		// Sets the hour
+	void setDoW(uint8_t DoW); 
+		// Sets the Day of the Week (1-7);
+	void setDate(uint8_t Date); 
+		// Sets the Date of the Month
+	void setMonth(uint8_t Month); 
+		// Sets the Month of the year
+	void setYear(uint8_t Year); 
+		// Last two digits of the year
+	void setClockMode(bool h12); 
+		// Set 12/24h mode. True is 12-h, false is 24-hour.
+
+/*=========================================================================*/
+
+/*=========================================================================
+    Temperature function.
+    -----------------------------------------------------------------------*/
+
+	float getTemperature(); 
+
+/*=========================================================================*/
+
+/*=========================================================================
+    Alarm functions.
+    -----------------------------------------------------------------------*/
+		
+	void getA1Time(uint8_t& A1Day, uint8_t& A1Hour, uint8_t& A1Minute, uint8_t& A1Second, 
+					uint8_t& AlarmBits, bool& A1Dy, bool& A1h12, bool& A1PM); 
+	
+	/* Retrieves everything you could want to know about alarm
+	 * one. 
+	 * A1Dy true makes the alarm go on A1Day = Day of Week,
+	 * A1Dy false makes the alarm go on A1Day = Date of month.
+	 *
+	 * byte AlarmBits sets the behavior of the alarms:
+	 *	Dy	A1M4	A1M3	A1M2	A1M1	Rate
+	 *	X	1		1		1		1		Once per second
+	 *	X	1		1		1		0		Alarm when seconds match
+	 *	X	1		1		0		0		Alarm when min, sec match
+	 *	X	1		0		0		0		Alarm when hour, min, sec match
+	 *	0	0		0		0		0		Alarm when date, h, m, s match
+	 *	1	0		0		0		0		Alarm when DoW, h, m, s match
+	 *
+	 *	Dy	A2M4	A2M3	A2M2	Rate
+	 *	X	1		1		1		Once per minute (at seconds = 00)
+	 *	X	1		1		0		Alarm when minutes match
+	 *	X	1		0		0		Alarm when hours and minutes match
+	 *	0	0		0		0		Alarm when date, hour, min match
+	 *	1	0		0		0		Alarm when DoW, hour, min match
+	 */
+
+	void setA1Time(uint8_t A1Day, uint8_t A1Hour, uint8_t A1Minute, uint8_t A1Second, 
+					uint8_t AlarmBits, bool A1Dy, bool A1h12, bool A1PM); 
+		// Set the details for Alarm 1
+	void turnOnAlarm(uint8_t Alarm); 
+		// Enables alarm 1 or 2 and the external interrupt pin.
+		// If Alarm != 1, it assumes Alarm == 2.
+	void turnOffAlarm(uint8_t Alarm); 
+		// Disables alarm 1 or 2 (default is 2 if Alarm != 1);
+		// and leaves the interrupt pin alone.
+	bool checkAlarmEnabled(uint8_t Alarm); 
+		// Returns T/F to indicate whether the requested alarm is
+		// enabled. Defaults to 2 if Alarm != 1.
+	bool checkIfAlarm(uint8_t Alarm); 
+		// Checks whether the indicated alarm (1 or 2, 2 default);
+		// has been activated.
+
+/*=========================================================================*/
+
+/*=========================================================================
+    Oscillator functions.
+    -----------------------------------------------------------------------*/
+
+	void enableOscillator(bool TF, bool battery, uint8_t frequency); 
+		// turns oscillator on or off. True is on, false is off.
+		// if battery is true, turns on even for battery-only operation,
+		// otherwise turns off if Vcc is off.
+		// frequency must be 0, 1, 2, or 3.
+		// 0 = 1 Hz
+		// 1 = 1.024 kHz
+		// 2 = 4.096 kHz
+		// 3 = 8.192 kHz 
+	void enable32kHz(bool TF); 
+		// Turns the 32kHz output pin on (true) or off (false).
+	bool oscillatorCheck(void);
+		// Checks the status of the OSF (Oscillator Stop Flag).
+		// If this returns false, then the clock is probably not
+		// giving you the correct time.
+		// The OSF is cleared by function setSecond().
+			
+/*=========================================================================*/
+
+/*=========================================================================
+    Converting functions.
+    -----------------------------------------------------------------------*/
+
+	uint8_t decToBcd(uint8_t val);
+		// Convert normal decimal numbers to binary coded decimal
+	uint8_t bcdToDec(uint8_t val);
+		// Convert binary coded decimal to normal decimal numbers
+	
+/*=========================================================================*/
+
+/*=========================================================================
+    Operation functions.
+    -----------------------------------------------------------------------*/	
+	
+	uint8_t readControlByte(bool which);
+		// Read selected control byte: (0); reads 0x0e, (1) reads 0x0f
+	void writeControlByte(uint8_t control, bool which);
+		// Write the selected control byte.
+		// which == false -> 0x0e, true->0x0f.
+
+/*=========================================================================*/
+#endif
